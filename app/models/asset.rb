e20c879aa89dabec950499d69d4f3dfb742c5fb1
David Rose <david@sxsw.com>
2008-10-15 15:46:09 -0500

    corrected misspelled method invocation.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
   acts_as_taggable
   has_attached_file :asset, :styles => { :thumbnail => '200x200>' }
-  validate_presence_of :asset, :contributor_email
+  validates_presence_of :asset, :contributor_email
 end
