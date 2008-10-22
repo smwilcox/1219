aa7898df25b0aad72626453991fa67d54c65cd38
David Rose <david@sxsw.com>
2008-10-22 11:18:34 -0500

    added index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :assets
-  map.root :controller =>  'assets'
   
   # The priority is based upon order of creation: first created -> highest priority.
 
